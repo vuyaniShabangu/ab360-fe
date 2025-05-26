1896296c94aa6cc130f965b6687fa3996feb4665
Aurelian-Nkonde <aurelianrobot@gmail.com>
2025-05-27 00:07:15 +0200

    invite sign up and call the endpoint when invite is sent

---
+++ src/app/accept-invitation/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { AcceptInvitationForm } from "@/components/accept-invitation";
+
+
+export default async function AcceptInvitationPage({params}:{params: Promise<{id: string}>}) {
+
+  const {id } = await params;
+
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/onboarding.png"
+          alt="Adboss 360"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md">
+              <img src="/logo.svg" alt="Adboss 360" />
+            </div>
+            360 &deg;
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <AcceptInvitationForm id={id}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
