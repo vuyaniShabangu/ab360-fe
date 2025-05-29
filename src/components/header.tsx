8a8f7c264e1a3bbcf80d52852e299342c4ace162
Aurelian-Nkonde <aurelianrobot@gmail.com>
2025-05-29 17:23:06 +0200

    cleaning up code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,8 +23,6 @@ export function Header() {
     <header className="sticky top-0 z-10 flex h-16 w-full items-center justify-end border-b bg-background px-6 shadow-sm">
       <nav className="flex items-center gap-6">
         <ClientProjectSelect />
-       
-
         <Avatar className="h-9 w-9">
           <AvatarImage src="/placeholder.svg?height=36&width=36" alt="User" />
           <AvatarFallback>{userName.charAt(0).toUpperCase()}</AvatarFallback>
